25dae92533019a265f473683a0d9348a50515411
vamshidharkasulabada <kasulavamshidhar8@gmail.com>
2022-08-21 11:21:01 +0530

    Added Dark Theme

--- app/src/main/java/com/example/quicknote/ui/notesScreen/NotesFragment.kt
+++ app/src/main/java/com/example/quicknote/ui/notesScreen/NotesFragment.kt
@@ -1,10 +1,8 @@
 package com.example.quicknote.ui.notesScreen
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.activity.viewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -60,6 +58,7 @@ class NotesFragment : Fragment() {
             }
         })
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
